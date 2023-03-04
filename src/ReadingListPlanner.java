dd488edad719de7dd12a8e40c6f5cd5561324982
leon23652 <86147415+leon23652@users.noreply.github.com>
2023-03-04 19:54:11 +0000

    fix a few errors

--- src/ReadingListPlanner.java
+++ src/ReadingListPlanner.java
@@ -30,58 +30,130 @@ public class ReadingListPlanner {
 
             // Get items from authorStore
 
-            for (char c : input.toCharArray()) {
-                String character = Character.toString(c);
-
-                if (result.size() < 1) {
-                    String author = authorStore.getRandomItem(String.valueOf(c));
-                    result.add(author);
-
-                }
-
-
-                // Get items from prizeWinnerStore
-                else if (result.size() < 2) {
-                    String prizeWinner = prizeWinnerStore.getRandomItem(character);
-                    result.add(prizeWinner);
-
-                } else if (numOfPrize < numOfBook) {
-                    String prizeWinner = prizeWinnerStore.getRandomItem(character);
-                    result.add(prizeWinner);
-
-
-                } else {
-                    String Book = bookStore.getRandomItem(character);
-                    result.add(Book);
-                    numOfBook++;
-                }
-
-            }
-            return result;
+            return getStrings(input, result, numOfPrize, numOfBook);
         } else {
-            // generate with prefix length
-            int length = Integer.parseInt(prefixLength);
-            for (int i = 0; i < input.length(); i+=length) {
-                String author = authorStore.getRandomItem(input);
-                String prizeWinner = prizeWinnerStore.getRandomItem(input);
-                String Book = bookStore.getRandomItem(input);
 
-
-
-            }
-
-            return result;
-
+            return getString(input, result, numOfPrize, numOfBook, Integer.parseInt(prefixLength));
         }
     }
 
+    private List<String> getStrings(String input, List<String> result, int numOfPrize, int numOfBook) {
+        for (char c : input.toCharArray()) {
+            String character = Character.toString(c);
 
+            if (result.size() < 1) {
+                String author = authorStore.getRandomItem(character);
+                result.add(author);
+
+            }
+
+
+            // Get items from prizeWinnerStore
+            else if (result.size() < 2) {
+                String prizeWinner = prizeWinnerStore.getRandomItem(character);
+
+                result.add(prizeWinner);
+
+            } else if (numOfPrize < numOfBook) {
+                String prizeWinner = prizeWinnerStore.getRandomItem(character);
+                result.add(prizeWinner);
+
+
+            } else {
+                String Book = bookStore.getRandomItem(character);
+                result.add(Book);
+                numOfBook++;
+            }
+
+        }
+        return result;
+    }
+    private List<String> getString(String input, List<String> result, int numOfPrize, int numOfBook, int prefixLength ) {
+        for (char c : input.toCharArray()) {
+            int charCount = 1;
+            String character = Character.toString(c);
+
+            if (result.size() < 1) {
+                String author = authorStore.getRandomItem(character);
+                int x = 1;
+
+
+                while ((charCount < input.length()-1)&&(author.substring(0,x).equalsIgnoreCase(input.substring(0, charCount)))){
+
+
+                    charCount+=1;
+                    x = x+1;
+                    }
+                author = author.substring(0, x).toUpperCase() + author.substring(x);
+                result.add(author);
+
+
+                }
+
+
+
+
+
+
+
+            // Get items from prizeWinnerStore
+            else if (result.size() < 2) {
+
+                String prizeWinner = prizeWinnerStore.getRandomItem(character);
+                int x = 1;
+
+                while ((charCount < input.length()-1)&&(prizeWinner.substring(0,x).equalsIgnoreCase(input.substring(0, charCount)))){
+
+
+                    charCount+=1;
+                    x = x+1;
+                }
+                prizeWinner = prizeWinner.substring(0, x).toUpperCase() + prizeWinner.substring(x);
+
+                result.add(prizeWinner);
+
+            } else if (numOfPrize < numOfBook) {
+                String prizeWinner = prizeWinnerStore.getRandomItem(character);
+                int x = 1;
+
+                while ((charCount < input.length()-1)&&(prizeWinner.substring(0,x).equalsIgnoreCase(input.substring(0, charCount)))){
+
+                    charCount+=1;
+                    x = x+1;
+                }
+                prizeWinner = prizeWinner.substring(0, x).toUpperCase() + prizeWinner.substring(x);
+
+                result.add(prizeWinner);
+
+
+
+            } else {
+
+                String Book = bookStore.getRandomItem(character);
+                int x = 1;
+
+                while ((charCount < input.length()-1)&&(Book.substring(0,x).equalsIgnoreCase(input.substring(0, charCount)))){
+
+
+                    charCount+=1;
+                    x = x+1;
+                }
+                Book = Book.substring(0, x).toUpperCase() + Book.substring(x);
+                result.add(Book);
+
+            }
+
+        }
+        return result;
+    }
 
 
     public static void main(String[] args) throws IOException {
+        //args = new String[]{"thththththththththth", "2"};
 
 
-try {
+
+        try {
     ReadingListItemStore authorStore = new ReadingListItemStore("authors.txt",(Integer.parseInt(args[1])));
     PrizeWinnerStore prizeWinnerStore = new PrizeWinnerStore("prize-winners.txt", (Integer.parseInt(args[1])));
     BookStore bookStore = new BookStore("books.txt");
